cb588381377bc39849f070797dfd519454002835
David B <d.bonilla@edu.uah.es>
2025-10-14 13:15:59 +0200

    Editorial.h y main.cpp
    probar si funciona la pila

--- editorial.h
+++ editorial.h
@@ -1,1 +1,32 @@
+#include EDITORIAL.H
+#define EDITORIAL.H
+#include <iostream>
 
+class NodoPila
+{
+    private:
+        int valor;
+        NodoPila *siguiente;
+        friend class Pila;
+    public:
+        NodoPila();
+        NodoPila(int v, NodoPila *sig = NULL);
+        ~NodoPila();
+};
+typedef NodoPila *pnodo;
+
+
+class Pila
+{
+    private:
+        pnodo cima;
+    public:
+        Pila();
+        ~Pila();
+        bool esVacia();
+        void apilar(int v);
+        void desapilar();
+        int mostrar();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-
+#include "editorial.h"
+#include "editorial.cpp"
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "HOLA" << endl;
-    cout << "Adios" <<endl;
-    cout << "Prueba" <<endl;
+    cout << "Creamos la pila Vacía" << endl;
+    Pila p; p.mostrar();
+    cout << "Apilo 0" << endl;
+    p.apilar(0); p.mostrar();
+    cout << "Apilo 1" << endl;
+    p.apilar(1); p.mostrar();
+    cout << "Apilo 2" << endl;
+    p.apilar(2); p.mostrar();
+    cout << "Desapilo" << endl;
+    p.desapilar(); p.mostrar();
+    p.~Pila(); //es innecesario, se va a llamar al destructor al salir de la ejecución
     return 0;
 }
