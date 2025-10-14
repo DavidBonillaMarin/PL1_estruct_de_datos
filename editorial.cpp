7b877a666ad9eaf63fe37a415292c9dd143c0de4
AlvaroGonzalezLopezUAH <alvarogonzalezlopez06@gmail.com>
2025-10-14 13:16:01 +0200

    Update editorial.cpp
    Editorial.cpp con las funciones de la pila

--- editorial.cpp
+++ editorial.cpp
@@ -3,14 +3,14 @@
 
 NodoPila::NodoPila()
 {
-valor=0;
-siguiente=NULL;
+    valor=0;
+    siguiente=NULL;
 //ctor
 }
 NodoPila::NodoPila(int v, NodoPila *sig)
 {
-valor = v;
-siguiente = sig;
+    valor = v;
+    siguiente = sig;
 }
 NodoPila::~NodoPila()
 {
@@ -18,29 +18,31 @@ NodoPila::~NodoPila()
 }
 Pila::Pila()
 {cima = NULL;}
+
 Pila::~Pila()
 { while(cima) desapilar();}
+
 bool Pila::esVacia()
 { return cima == NULL; }
+
 void Pila::apilar(int v)
-{ pnodo nuevo = new NodoPila(v,
-cima);
+{ pnodo nuevo = new NodoPila(v, cima);
 //comienzo de la pila nevo nodo
-cima = nuevo;
+    cima = nuevo;
 }
 void Pila::desapilar()
 { pnodo nodo; //puntero aux para manipular el nodo
-if(cima)
-nodo = cima;
-cima = nodo->siguiente;
-delete nodo;
+    if(cima)
+        nodo = cima;
+        cima = nodo->siguiente;
+    delete nodo;
 }
 int Pila::mostrar()
-{ if(esVacia()) {
-cout << "Pila vacia"<<endl;
+{   if(esVacia()) {
+        cout << "Pila vacia"<<endl;
 }
-else{
-cout << "Cima pila: "<< cima->valor<<endl;
+    else{
+        cout << "Cima pila: "<< cima->valor<<endl;
 }
-return 0;
+        return 0;
 }
